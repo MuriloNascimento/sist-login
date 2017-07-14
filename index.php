37f6851931b1d437caa84ab9e45a05ed4bca2d75
Murilo Nascimento <murilo.nascimento1988@gmail.com>
2017-07-14 10:26:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,10 @@
 		<link rel="stylesheet" type="text/css" href="css/stilo.css">
 		<title>teste de login</title>
 		
-		<script src="https://www.gstatic.com/firebasejs/4.1.3/firebase.js"></script>
+		<script src="https://www.gstatic.com/firebasejs/3.8.0/firebase-app.js"></script>
+		<script src="https://www.gstatic.com/firebasejs/3.8.0/firebase-auth.js"></script>
+		<script src="https://www.gstatic.com/firebasejs/3.8.0/firebase-database.js"></script>
+		<script src="https://www.gstatic.com/firebasejs/3.8.0/firebase-messaging.js"></script>
 		<script>
 		    // Initialize Firebase
 			
