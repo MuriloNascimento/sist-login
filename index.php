5cc297548c06bf3d3e1d52620920e2fe74bfc1ca
Murilo Nascimento <murilo.nascimento1988@gmail.com>
2017-07-14 09:55:11 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,38 @@
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="css/stilo.css">
 		<title>teste de login</title>
+		
+		<script src="https://www.gstatic.com/firebasejs/4.1.3/firebase.js"></script>
+		<script>
+		    // Initialize Firebase
+			console.log('teste');
+		    var config = {
+			apiKey: "AIzaSyDOhdzJdYkrPz9yw4e694SDq0tRWecbpYc",
+			authDomain: "app-portal-revenda.firebaseapp.com",
+			databaseURL: "https://app-portal-revenda.firebaseio.com",
+			projectId: "app-portal-revenda",
+			storageBucket: "app-portal-revenda.appspot.com",
+			messagingSenderId: "761472737871"
+		    };
+		    firebase.initializeApp(config);
+
+		    const messaging = firebase.messaging();
+
+		    messaging.requestPermission().then(function() {
+			console.log('Notification permission granted.');
+			messaging.getToken().then(function(currentToken) {
+			    if (currentToken) {
+
+			    } else {
+				console.log('No Instance ID token available. Request permission to generate one.');
+			    }
+			}).catch(function(err) {
+
+			});
+		    }).catch(function(err) {
+			console.log('Unable to get permission to notify.', err);
+		    });
+		</script>
 	</head>
 	<body>
 		<header>
@@ -24,37 +56,4 @@
 			</div>
 		</section>
 	</body>
-	
-	
-	<script src="https://www.gstatic.com/firebasejs/4.1.3/firebase.js"></script>
-        <script>
-            // Initialize Firebase
-		console.log('teste');
-            var config = {
-                apiKey: "AIzaSyDOhdzJdYkrPz9yw4e694SDq0tRWecbpYc",
-                authDomain: "app-portal-revenda.firebaseapp.com",
-                databaseURL: "https://app-portal-revenda.firebaseio.com",
-                projectId: "app-portal-revenda",
-                storageBucket: "app-portal-revenda.appspot.com",
-                messagingSenderId: "761472737871"
-            };
-            firebase.initializeApp(config);
-
-            const messaging = firebase.messaging();
-
-            messaging.requestPermission().then(function() {
-                console.log('Notification permission granted.');
-                messaging.getToken().then(function(currentToken) {
-                    if (currentToken) {
-                        
-                    } else {
-                        console.log('No Instance ID token available. Request permission to generate one.');
-                    }
-                }).catch(function(err) {
-
-                });
-            }).catch(function(err) {
-                console.log('Unable to get permission to notify.', err);
-            });
-        </script>
 </html>
