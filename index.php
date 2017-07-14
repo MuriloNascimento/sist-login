e702b0a8afcde413371110a517e368d45ed815bb
Murilo Nascimento <murilo.nascimento1988@gmail.com>
2017-07-14 10:06:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -24,18 +24,45 @@
 
 		    messaging.requestPermission().then(function() {
 			console.log('Notification permission granted.');
-			messaging.getToken().then(function(currentToken) {
+			messaging.getToken()
+			  .then(function(currentToken) {
 			    if (currentToken) {
-				console.log(currentToken);
+				    console.log(currentToken);
+			      sendTokenToServer(currentToken);
+			      updateUIForPushEnabled(currentToken);
 			    } else {
-				console.log('No Instance ID token available. Request permission to generate one.');
+			      // Show permission request.
+			      console.log('No Instance ID token available. Request permission to generate one.');
+			      // Show permission UI.
+			      updateUIForPushPermissionRequired();
+			      setTokenSentToServer(false);
 			    }
-			}).catch(function(err) {
-
-			});
+			  })
+			  .catch(function(err) {
+			    console.log('An error occurred while retrieving token. ', err);
+			    showToken('Error retrieving Instance ID token. ', err);
+			    setTokenSentToServer(false);
+			  });
 		    }).catch(function(err) {
 			console.log('Unable to get permission to notify.', err);
 		    });
+			
+			messaging.onTokenRefresh(function() {
+			  messaging.getToken()
+			  .then(function(refreshedToken) {
+			    console.log('Token refreshed.');
+			    // Indicate that the new Instance ID token has not yet been sent to the
+			    // app server.
+			    setTokenSentToServer(false);
+			    // Send Instance ID token to app server.
+			    sendTokenToServer(refreshedToken);
+			    // ...
+			  })
+			  .catch(function(err) {
+			    console.log('Unable to retrieve refreshed token ', err);
+			    showToken('Unable to retrieve refreshed token ', err);
+			  });
+			});
 		</script>
 	</head>
 	<body>
