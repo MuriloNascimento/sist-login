48d188cc4a7ed4db0fc8e750a6e2cd55668ef756
Murilo Nascimento <murilo.nascimento1988@gmail.com>
2017-11-16 08:20:46 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,12 @@
 <html>
-	<head>
-	<script async="async" src="https://www.googletagservices.com/tag/js/gpt.js">
-	</script>
-	<script>
-	  var googletag = googletag || {};
-	  googletag.cmd = googletag.cmd || [];
-	</script>
-	<script>
-	  googletag.cmd.push(function() {
-	    googletag.pubads().set("adsense_background_color", "FFFFFF");
-	  });
-	</script>
-	<script>
-	  googletag.cmd.push(function() {
-	    googletag.defineSlot("/1234/travel/asia", [728, 90], "div-gpt-ad-123456789-0")
+<head>
+		<script src="https://www.googletagservices.com/tag/js/gpt.js"> 
+		</script>
+		<script type="text/javascript">
+		    googletag.pubads().set("adsense_background_color", "FFFFFF");
+		</script>
+		<script>
+		    googletag.defineSlot("/1234/travel/asia", [728, 90], "div-gpt-ad-123456789-0")
 	      .addService(googletag.pubads())
 	      .setTargeting("interests", ["sports", "music", "movies"]);
 	    googletag.defineSlot("/1234/travel/asia", [[468, 60], [728, 90], [300, 250]], "div-gpt-ad-123456789-1")
@@ -22,24 +15,20 @@
 	      .setTargeting("age", "20-30");
 	    googletag.pubads().setTargeting("topic","basketball");
 	    googletag.pubads().enableSingleRequest();
+	    googletag.pubads().enableSyncRendering();
 	    googletag.enableServices();
-	  });
 	</script>
 	</head>
 	<body>
 	  <div id="div-gpt-ad-123456789-0" style="width: 728px; height: 90px">
-	    <script>
-	      googletag.cmd.push(function() {
-	         googletag.display("div-gpt-ad-123456789-0");
-	      });
+	    <script type="text/javascript">
+	      googletag.display("div-gpt-ad-123456789-0");
 	    </script>
 	  </div>
-	    <div id="div-gpt-ad-123456789-1">
-	    <script>
-	      googletag.cmd.push(function() {
-	         googletag.display("div-gpt-ad-123456789-1");
-	      });
+	  <div id="div-gpt-ad-123456789-1">
+	    <script type="text/javascript">
+	      googletag.display("div-gpt-ad-123456789-1");
 	    </script>
 	  </div>
 	</body>
-	</html>
+</html>
